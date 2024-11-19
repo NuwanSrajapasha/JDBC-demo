c286e194b3c969a3df64e972848f2dcfebd670f1
NuwanS rajapaksha <155893660+NuwanSrajapasha@users.noreply.github.com>
2024-11-19 10:41:06 +0530

    Insert data

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
-    public static void main(String[] args)  {
-        Class.forName("com.mysql.cj.jdbc.Driver")
+    public static void main(String[] args) throws ClassNotFoundException , SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "mysql1");
+        
+        String sql = "INSERT INTO Customer VALUES ('C033', 'Mrs', 'Kamala', '1976-12-15', 100000.00, 'Panadura', 'Panadura', 'Western', '70000')";
+        Statement statement = connection.createStatement();
+
+        int result=statement.executeUpdate(sql);
+        System.out.println(result > 0? "Success" : "Fail");
+
+
     }
 }
